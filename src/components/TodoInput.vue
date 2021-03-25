d30ce4bd0545e94c25e48e1497471c6ff8d3bd60
choijinwon <abyys83@gmail.com>
2021-03-26 03:44:31 +0900

    inputbox 연동

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <label for="todo-Item"></label>
+    <input id="todo-Item" type="text" />
+    <button type="button">추가하기</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style scoped></style>
