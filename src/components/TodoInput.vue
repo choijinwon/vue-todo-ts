b0553973069dd83339810e6df1e90ca1e73f3d20
choijinwon <abyys83@gmail.com>
2021-03-26 03:46:55 +0900

    클릭버튼

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -2,14 +2,20 @@
   <div>
     <label for="todo-Item"></label>
     <input id="todo-Item" type="text" />
-    <button type="button">추가하기</button>
+    <button type="button" @click="addTodo">추가하기</button>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 
-export default Vue.extend({});
+export default Vue.extend({
+  methods: {
+    addTodo() {
+      console.log("클릭");
+    },
+  },
+});
 </script>
 
 <style scoped></style>
